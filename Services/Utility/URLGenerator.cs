1f2af1f6ac59310d463675fc997793f44b185348
origo patrick-ivann <patrickivann.origo@viacesi.fr>
2020-10-17 23:59:22 +0200

    :bug: Fix out of range on GenerateShortUrl
    * when the offset is to important and length greater than string length

--- Services/Utility/URLGenerator.cs
+++ Services/Utility/URLGenerator.cs
@@ -9,14 +9,19 @@ namespace rapide_shortener_service.Services.Utility
     {
         public static string GenerateShortUrl()
         {
-            string urlsafe = string.Empty;
+            string urlSafe = string.Empty;
             Enumerable.Range(48, 75)
               .Where(i => i < 58 || i > 64 && i < 91 || i > 96)
               .OrderBy(o => new Random().Next())
               .ToList()
-              .ForEach(i => urlsafe += Convert.ToChar(i));
-            string token = urlsafe.Substring(new Random().Next(0, urlsafe.Length), new Random().Next(2, 6));
-
+              .ForEach(i => urlSafe += Convert.ToChar(i));
+            urlSafe = String.Join("", urlSafe.ToCharArray().OrderBy(x => Guid.NewGuid()).Select(c => c.ToString()).ToArray());
+            int offset = new Random().Next(0, urlSafe.Length - 2);
+            int length = new Random().Next(2, urlSafe.Length - offset);
+            if (length > 10) length = length - 6;
+            if (length > 20) length = length - 14;
+            if (length > 7) length = length - 5;
+            string token = urlSafe.Substring(offset, length);
             return "https://share.lunaar.net/" + token;
 
         }
